47c4ccd234478bce1cdc5fe4d0177df51cbfe942
Meraj Alam <merajsiddieque@gmail.com>
2024-10-09 22:08:55 +0530

    init

--- tender/src/testing/test.java
+++ tender/src/testing/test.java
@@ -5,6 +5,10 @@ public class test {
 	public static void main(String[] args)
 	{
        System.out.println("hii");
+       System.out.println("hii");
+       System.out.println("hii");
+       System.out.println("hii");
+       System.out.println("hii");
 	}
 
 }
