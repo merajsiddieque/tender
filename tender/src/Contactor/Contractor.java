3d232c485d4daec0e9dfddf08ac5f4490a81f47e
ASUS <ASUS@MIHIR-07>
2024-10-10 22:19:41 +0530

    init

--- tender/src/Contactor/Contractor.java
+++ tender/src/Contactor/Contractor.java
@@ -5,6 +5,8 @@ public class Contractor {
 	public static void main(String[] args) 
 	{
        System.out.println("Hii Contactor");
+       System.out.println("Hii Contactor");
+       System.out.println("Hii Contactor");
 	}
 
 }
